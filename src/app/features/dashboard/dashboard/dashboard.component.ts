80ecf3cd08056bce467091e0e510bf9bad099b69
Sumukesh <amanchasumukesh08@gmail.com>
2022-11-15 17:04:09 +0530

    Basic Modification in dash board

--- src/app/features/dashboard/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  courseForm!: FormGroup;
+  overallReport: any;
+  coursesReport: any;
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit(): void {
+    this.courseForm = this.formBuilder.group({
+      roleId: [null, Validators.required]
+    });
+    this.overallReport = {
+      registeredUsers: 18547,
+      donationRecieved: 54785960,
+      courses: 15,
+      subscribers: 2015,
+    }
+    this.coursesReport = {
+      enrolledUsers: 18547,
+      activeUser: 2541,
+      droppedUsers: 15,
+      mentor: 57,
+      cheifMentor: 57,
+    }
   }
 
 }
