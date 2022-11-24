257d1897c1184bd88e4f190e861fe8baa347f830
Ajithkumar1810 <ajithkumar.k@bigtappanalytics.net>
2022-11-24 16:47:18 +0530

    Donation Management

--- src/app/features/donation-management/donation-management/donation-management.component.ts
+++ src/app/features/donation-management/donation-management/donation-management.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-donation-management',
@@ -7,10 +9,29 @@ import {MatDatepickerModule} from '@angular/material/datepicker';
   styleUrls: ['./donation-management.component.css']
 })
 export class DonationManagementComponent implements OnInit {
-
-  constructor() { }
+  dateForm!:FormGroup
+  disableSelect = new FormControl(false);
+  category:any=[{'S_No':'1', 'Date':'12-11-2022', 'DonorName':"ajith","Country":"India","Amountdonated":"10,500.00"},
+  {'S_No':'1', 'Date':'12-11-2022', 'DonorName':"ajith","Country":"India","Amountdonated":"10,500.00"},
+  {'S_No':'1', 'Date':'12-11-2022', 'DonorName':"ajith","Country":"India","Amountdonated":"10,500.00"},
+  {'S_No':'1', 'Date':'12-11-2022', 'DonorName':"ajith","Country":"India","Amountdonated":"10,500.00"}]
+  displayedColumns: string[] = ['S_No', 'Date', 'DonorName',"Country","Amountdonated","ViewDetails"];
+total="1,20,52,365"
+ 
+  dataSource :any;
+  constructor(private formbuilder:FormBuilder) { }
 
   ngOnInit(): void {
+    this.dateForm = this.formbuilder.group({
+      
+      fromdate: [null, Validators.compose([Validators.required])],
+      todate: [null, Validators.compose([Validators.required])],
+      
+      
+    })
+    this.dataSource=new MatTableDataSource<any>(this.category)
   }
+  viewDetails(name:any){
 
+  }
 }
