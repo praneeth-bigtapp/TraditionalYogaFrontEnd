4b40bf0e89da8aa916a644c6c9862706c46605a7
Ajithkumar1810 <ajithkumar.k@bigtappanalytics.net>
2022-11-25 13:03:37 +0530

    Donation Management api2

--- src/app/features/donation-management/donation-management/donation-management.component.ts
+++ src/app/features/donation-management/donation-management/donation-management.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatTableDataSource } from '@angular/material/table';
 import { DonationserviceService } from '../service/donationservice.service';
 
@@ -10,45 +10,56 @@ import { DonationserviceService } from '../service/donationservice.service';
   styleUrls: ['./donation-management.component.css']
 })
 export class DonationManagementComponent implements OnInit {
-  dateForm!:FormGroup
+  dateForm!: FormGroup
   // disableSelect = new FormControl(false);
   // category:any=[{'S_No':'1', 'Date':'12-11-2022', 'DonorName':"ajith","Country":"India","Amountdonated":"10,500.00"},
   // {'S_No':'1', 'Date':'12-11-2022', 'DonorName':"ajith","Country":"India","Amountdonated":"10,500.00"},
   // {'S_No':'1', 'Date':'12-11-2022', 'DonorName':"ajith","Country":"India","Amountdonated":"10,500.00"},
   // {'S_No':'1', 'Date':'12-11-2022', 'DonorName':"ajith","Country":"India","Amountdonated":"10,500.00"}]
-  displayedColumns: string[] = ['S_No', 'Date', 'DonorName',"Country","Amountdonated","ViewDetails"];
-total="1,20,52,365"
- 
-  dataSource :any;
-  data:any;
-  constructor(private formbuilder:FormBuilder, private service:DonationserviceService) { }
+  displayedColumns: string[] = ['S_No', 'Date', 'DonorName', "Country", "Amountdonated", "ViewDetails"];
+  total = 135564564;
+
+  dataSource: any;
+  data: any;
+  constructor(private formbuilder: FormBuilder, private service: DonationserviceService) { }
 
   ngOnInit(): void {
     this.dateForm = this.formbuilder.group({
-      
+
       fromdate: [null, Validators.compose([Validators.required])],
       todate: [null, Validators.compose([Validators.required])],
-      checkbox:[null, Validators.compose([Validators.required])],
-      
+      checkbox: [null, Validators.compose([Validators.required])],
+
     })
-   
+    this.getallData()
+
+
+  }
+  getallData() {
     this.service.getdonationdetails().subscribe({
       next: (response) => {
         this.data = response;
         this.dataSource = new MatTableDataSource<any>(this.data);
-        console.log(this.data)
+        
       },
       error: (error) => {
         console.error(error.message);
       }
     });
+  }
+  viewDetails(name: any) {
 
   }
-  viewDetails(name:any){
-
-  }
-  filterDate(){
+  filterDate() {
+    const fromdate = this.dateForm.value.fromdate
+    const todate = this.dateForm.value.todate
+    let filters = this.data.filter((ele: any) => new Date(ele.date) >= new Date(fromdate) && new Date(ele.date) <= new Date(todate))
+    if(fromdate!==null && todate!==null){
+      this.dataSource = new MatTableDataSource<any>(filters);
+    }
+    
     
   }
+
 
 }
