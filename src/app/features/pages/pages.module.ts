049b4e4b1ada70aabea13efd300a114803f5d046
Ajithkumar1810 <ajithkumar.k@bigtappanalytics.net>
2022-11-30 17:26:12 +0530

    pages new

---
+++ src/app/features/pages/pages.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PagesRoutingModule } from './pages-routing.module';
+import { PagesComponent } from './pages/pages.component';
+
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+
+@NgModule({
+  declarations: [
+    PagesComponent
+  ],
+  imports: [
+    CommonModule,
+    PagesRoutingModule,
+    MatSortModule,
+    MatTableModule,
+    MatSelectModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatPaginatorModule,
+    MatDialogModule,
+    MatSlideToggleModule,
+    SharedModule,
+    MatTooltipModule,
+    MatIconModule,
+  ]
+})
+export class PagesModule { }
---
+++ src/app/features/pages/pages/pages.component.ts
@@ -0,0 +1,70 @@
+import { SelectionModel } from '@angular/cdk/collections';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
+@Component({
+  selector: 'app-pages',
+  templateUrl: './pages.component.html',
+  styleUrls: ['./pages.component.css']
+})
+export class PagesComponent implements OnInit {
+  @ViewChild(MatPaginator, { static: false }) paginator!: MatPaginator;
+  @ViewChild(MatSort, { static: false }) sort!: MatSort;
+  selection = new SelectionModel<any>(true, []);
+data=[{"Pagename":"About US","Author":'admin','Dateofpublish':'02,Aug,2022, 12:30:37',"link":"https://tradiationalyoga.co.in/about"},
+{"Pagename":"About US","Author":'admin','Dateofpublish':'02,Aug,2022, 12:30:37',"link":"https://tradiationalyoga.co.in/about"},
+{"Pagename":"About US","Author":'admin','Dateofpublish':'02,Aug,2022, 12:30:37',"link":"https://tradiationalyoga.co.in/about"},
+{"Pagename":"About US","Author":'admin','Dateofpublish':'02,Aug,2022, 12:30:37',"link":"https://tradiationalyoga.co.in/about"},
+{"Pagename":"About US","Author":'admin','Dateofpublish':'02,Aug,2022, 12:30:37',"link":"https://tradiationalyoga.co.in/about"},
+{"Pagename":"About US","Author":'admin','Dateofpublish':'02,Aug,2022, 12:30:37',"link":"https://tradiationalyoga.co.in/about"}]
+filterData:any
+gridData :any;
+
+
+dataSource: any;
+displayedColumns: string[] = ["Checkbox","Pagename","Author",'Dateofpublish'];
+
+  constructor(private formbuilder:FormBuilder) { }
+
+  ngOnInit(): void {
+    this.filterData = {
+      filterColumnNames: this.displayedColumns.map(ele => ({ "Key": ele, "Value": "" })),
+      gridData: this.gridData,
+      dataSource: this.dataSource,
+      paginator: this.paginator,
+      sort: this.sort
+    }
+    this.dataSource = new MatTableDataSource<any>(this.data)
+    this.filterData.gridData = this.data;
+    this.filterData.dataSource = this.dataSource;
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+    this.filterData.sort = this.sort;
+    for (let col of this.filterData.filterColumnNames) {
+      col.Value = '';
+    }
+    }
+    viewDetails(element:any){
+  
+    }
+    updatePagination() {
+  
+      this.dataSource.paginator = this.paginator;
+    }
+    
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.filterData.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  masterToggle() {
+    this.isAllSelected() ?
+      this.selection.clear() :
+      this.filterData.dataSource.data.forEach((row: any) => this.selection.select(row));
+  }
+
+}
