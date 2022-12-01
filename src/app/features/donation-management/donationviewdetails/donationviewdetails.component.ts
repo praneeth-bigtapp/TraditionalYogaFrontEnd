22f599ba56f2ec8a0ba79db6b46336c1c28548a9
Karthikeyan-FullStack-Developer <karthikeyan.d@bigtappanalytics.net>
2022-12-01 09:08:33 +0530

    view donation UI change

--- src/app/features/donation-management/donationviewdetails/donationviewdetails.component.ts
+++ src/app/features/donation-management/donationviewdetails/donationviewdetails.component.ts
@@ -12,14 +12,18 @@ export class DonationviewdetailsComponent implements OnInit {
   details = {
     amount: "",
     donorname: "",
+    address: "",
     email: "",
     mobile: "",
+    isIndian: "",
+    taxbenefits: "",
+    identitynumber: "",
     isregisteredmember: "",
     country: "",
     message: "",
     modeofpayment: "",
+    cardname: "",
     identityproof: ""
-
   }
   apidata!: any
 
@@ -47,22 +51,41 @@ export class DonationviewdetailsComponent implements OnInit {
 
         this.apidata = response
 
-        const { amountDonated, modeOfPayment, description, identityproof } = this.apidata
-        const { registedIpAddress, name, mobile, address, emailId } = this.apidata.student
+        const { amountDonated, country, identitynumber, cardname, taxbenefits, modeOfPayment, description, identityproof, isIndian, registedIpAddress, name, mobile, address, emailId } = this.apidata
         this.details.amount = amountDonated || "NIL"
-        this.details.modeofpayment = modeOfPayment || "NIL"
-        this.details.message = description || "NIL"
         this.details.donorname = name || "NIL"
+        this.details.address = address || "NIL"
         this.details.email = emailId || "NIL"
         this.details.mobile = mobile || "NIL"
-        this.details.isregisteredmember = registedIpAddress || "NIL"
-        this.details.country = address || "NIL"
+        this.details.isIndian = isIndian || "NIL"
+        this.details.taxbenefits = taxbenefits || "NIL"
         this.details.identityproof = identityproof || "NIL"
+        this.details.identitynumber = identitynumber || "NIL"
+        this.details.isregisteredmember = registedIpAddress || "NIL"
+        this.details.country = country || "NIL"
+        this.details.modeofpayment = modeOfPayment || "NIL"
+        this.details.cardname = cardname || "NIL"
+
+        this.details.message = description || "NIL"
 
       },
       error: (error) => {
         console.error(error.message);
 
+        this.details.amount = "20000"
+        this.details.donorname = "donorname"
+        this.details.address = "address"
+        this.details.email = "email"
+        this.details.mobile = "mobile"
+        this.details.isIndian = " isIndian"
+        this.details.taxbenefits = "taxbenefits"
+        this.details.identityproof = "identityproof"
+        this.details.identitynumber = "identitynumber"
+        this.details.isregisteredmember = "registedIpAddress"
+        this.details.country = "country"
+        this.details.modeofpayment = "modeOfPayment"
+        this.details.cardname = "cardname"
+        this.details.message = "description"
       }
     })
 
