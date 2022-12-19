bb543f999f5d838b46e4a35ce779fc56521d0ba9
Karthikeyan-FullStack-Developer <karthikeyan.d@bigtappanalytics.net>
2022-12-19 10:24:51 +0530

    Paginator UI

--- src/app/features/donation-management/donationviewdetails/donationviewdetails.component.ts
+++ src/app/features/donation-management/donationviewdetails/donationviewdetails.component.ts
@@ -36,7 +36,15 @@ export class DonationviewdetailsComponent implements OnInit {
   ngOnInit(): void {
     this.getData()
   }
+  fullform(value: any) {
+    const yes = ["Yes", "Y", "yes", "y"]
+    const no = ["No", "N", "no", "n"]
 
+    if (yes.includes(value))
+      return "Yes"
+
+    return "No"
+  };
 
   getData() {
 
@@ -51,22 +59,26 @@ export class DonationviewdetailsComponent implements OnInit {
 
         this.apidata = response
 
-        // const { amountDonated, country, identitynumber, cardname, taxbenefits, modeOfPayment, description, identityproof, isIndian, registedIpAddress, name, mobile, address, emailId } = this.apidata
+        const { amountDonated, modeOfPayment, date } = this.apidata
+
+        const { donarId, address, email, contactNumber, indianCitizen, country, taxBenefits, identityProof, identityNumber, donationAmount, stripeCreditCard, cardName, message } = this.apidata.donarId
+
         // this.details.amount = amountDonated || "NIL"
-        // this.details.donorname = name || "NIL"
+        // this.details.donorname = '' || "NIL"
         // this.details.address = address || "NIL"
-        // this.details.email = emailId || "NIL"
-        // this.details.mobile = mobile || "NIL"
-        // this.details.isIndian = isIndian || "NIL"
-        // this.details.taxbenefits = taxbenefits || "NIL"
-        // this.details.identityproof = identityproof || "NIL"
-        // this.details.identitynumber = identitynumber || "NIL"
-        // this.details.isregisteredmember = registedIpAddress || "NIL"
+        // this.details.email = email || "NIL"
+        // this.details.mobile = '' || "NIL"
+        // this.details.isIndian = this.fullform(indianCitizen) || "NIL"
+        // this.details.taxbenefits = this.fullform(taxBenefits) || "NIL"
+        // this.details.identityproof = identityProof || "NIL"
+        // this.details.identitynumber = identityNumber || "NIL"
+        // this.details.isregisteredmember = '' || "NIL"
         // this.details.country = country || "NIL"
         // this.details.modeofpayment = modeOfPayment || "NIL"
-        // this.details.cardname = cardname || "NIL"
+        // this.details.cardname = cardName || "NIL"
 
-        // this.details.message = description || "NIL"
+        // this.details.message = message || "NIL"
+
         this.details.amount = "20000"
         this.details.donorname = "ajith"
         this.details.address = "hyderabad"
@@ -81,26 +93,27 @@ export class DonationviewdetailsComponent implements OnInit {
         this.details.modeofpayment = "Credit"
         this.details.cardname = "ajith kumar"
         this.details.message = "description"
+
 
 
       },
       error: (error) => {
         console.error(error);
 
-        this.details.amount = "20000"
-        this.details.donorname = "ajith"
-        this.details.address = "hyderabad"
-        this.details.email = "ajith@gmail.com"
-        this.details.mobile = "9876543210"
-        this.details.isIndian = " Yes"
-        this.details.taxbenefits = "No"
-        this.details.identityproof = "Pan Card"
-        this.details.identitynumber = "GHYYS2222Z"
-        this.details.isregisteredmember = "Yes"
-        this.details.country = "India"
-        this.details.modeofpayment = "Credit"
-        this.details.cardname = "ajith kumar"
-        this.details.message = "description"
+        // this.details.amount = "20000"
+        // this.details.donorname = "ajith"
+        // this.details.address = "hyderabad"
+        // this.details.email = "ajith@gmail.com"
+        // this.details.mobile = "9876543210"
+        // this.details.isIndian = " Yes"
+        // this.details.taxbenefits = "No"
+        // this.details.identityproof = "Pan Card"
+        // this.details.identitynumber = "GHYYS2222Z"
+        // this.details.isregisteredmember = "Yes"
+        // this.details.country = "India"
+        // this.details.modeofpayment = "Credit"
+        // this.details.cardname = "ajith kumar"
+        // this.details.message = "description"
       }
     })
 
