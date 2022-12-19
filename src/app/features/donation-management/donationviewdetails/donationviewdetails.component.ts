6e750fb9818c68f53d8805ad72f9682a4fe5af0b
Karthikeyan-FullStack-Developer <karthikeyan.d@bigtappanalytics.net>
2022-12-19 13:20:44 +0530

    Cleared Space

--- src/app/features/donation-management/donationviewdetails/donationviewdetails.component.ts
+++ src/app/features/donation-management/donationviewdetails/donationviewdetails.component.ts
@@ -34,16 +34,10 @@ export class DonationviewdetailsComponent implements OnInit {
     private donationservice: DonationserviceService,
     private activeroute: ActivatedRoute
   ) {
-
-    activeroute.queryParams.subscribe(params => {
-      this.getData(params["id"])
-    })
-
-
+    this.getData(this.activeroute.snapshot.paramMap.get('id'))
   }
 
   ngOnInit(): void {
-    this.getData("3")
   }
   fullform(value: any) {
     const yes = ["Yes", "Y", "yes", "y"]
@@ -63,8 +57,6 @@ export class DonationviewdetailsComponent implements OnInit {
 
     this.donationservice.postdonationdetails(body).subscribe({
       next: (response) => {
-        console.log(response);
-
 
         this.apidata = response
 
@@ -72,36 +64,36 @@ export class DonationviewdetailsComponent implements OnInit {
 
         const { donarId, address, email, contactNumber, indianCitizen, country, taxBenefits, identityProof, identityNumber, donationAmount, stripeCreditCard, cardName, message } = this.apidata.donarId
 
-        // this.details.amount = amountDonated || "NIL"
-        // this.details.donorname = '' || "NIL"
-        // this.details.address = address || "NIL"
-        // this.details.email = email || "NIL"
-        // this.details.mobile = '' || "NIL"
-        // this.details.isIndian = this.fullform(indianCitizen) || "NIL"
-        // this.details.taxbenefits = this.fullform(taxBenefits) || "NIL"
-        // this.details.identityproof = identityProof || "NIL"
-        // this.details.identitynumber = identityNumber || "NIL"
-        // this.details.isregisteredmember = '' || "NIL"
-        // this.details.country = country || "NIL"
-        // this.details.modeofpayment = modeOfPayment || "NIL"
-        // this.details.cardname = cardName || "NIL"
+        this.details.amount = amountDonated || "NIL"
+        this.details.donorname = '' || "NIL"
+        this.details.address = address || "NIL"
+        this.details.email = email || "NIL"
+        this.details.mobile = contactNumber || "NIL"
+        this.details.isIndian = this.fullform(indianCitizen) || "NIL"
+        this.details.taxbenefits = this.fullform(taxBenefits) || "NIL"
+        this.details.identityproof = identityProof || "NIL"
+        this.details.identitynumber = identityNumber || "NIL"
+        this.details.isregisteredmember = '' || "NIL"
+        this.details.country = country || "NIL"
+        this.details.modeofpayment = modeOfPayment || "NIL"
+        this.details.cardname = cardName || "NIL"
 
-        // this.details.message = message || "NIL"
+        this.details.message = message || "NIL"
 
-        this.details.amount = "20000"
-        this.details.donorname = "ajith"
-        this.details.address = "hyderabad"
-        this.details.email = "ajith@gmail.com"
-        this.details.mobile = "9876543210"
-        this.details.isIndian = " Yes"
-        this.details.taxbenefits = "No"
-        this.details.identityproof = "Pan Card"
-        this.details.identitynumber = "GHYYS2222Z"
-        this.details.isregisteredmember = "Yes"
-        this.details.country = "India"
-        this.details.modeofpayment = "Credit"
-        this.details.cardname = "ajith kumar"
-        this.details.message = "description"
+        // this.details.amount = "20000"
+        // this.details.donorname = "ajith"
+        // this.details.address = "hyderabad"
+        // this.details.email = "ajith@gmail.com"
+        // this.details.mobile = "9876543210"
+        // this.details.isIndian = " Yes"
+        // this.details.taxbenefits = "No"
+        // this.details.identityproof = "Pan Card"
+        // this.details.identitynumber = "GHYYS2222Z"
+        // this.details.isregisteredmember = "Yes"
+        // this.details.country = "India"
+        // this.details.modeofpayment = "Credit"
+        // this.details.cardname = "ajith kumar"
+        // this.details.message = "description"
 
 
 
