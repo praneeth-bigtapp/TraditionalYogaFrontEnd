d2b6cc540e104831b502fe1513b8e682d3e809f5
Karthikeyan-FullStack-Developer <karthikeyan.d@bigtappanalytics.net>
2023-01-04 09:50:39 +0530

    mentor module created

---
+++ src/app/mentormodule/viewstudent/viewstudent.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ViewstudentRoutingModule } from './viewstudent-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ViewstudentRoutingModule
+  ]
+})
+export class ViewstudentModule { }
