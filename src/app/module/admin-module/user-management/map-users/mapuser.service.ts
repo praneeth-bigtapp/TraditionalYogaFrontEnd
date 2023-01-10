a9afad30d4fe692d92bffe3e9e905b382bc4bb31
AJITHBT <ajith.r@bigtappanalytics.com>
2023-01-10 20:03:33 +0800

    changes

--- src/app/module/admin-module/user-management/map-users/mapuser.service.ts
+++ src/app/module/admin-module/user-management/map-users/mapuser.service.ts
@@ -7,11 +7,15 @@ import { BaseHttp } from 'src/app/core/services/baseHttp.service';
 export class MapuserService extends BaseHttp {
   mapcourse = 'courseList/mapCourses?operation=add';
   getcourseURL="courseList/getAll?operation=coursesList";
+  getusers='register/getAll?operation=registerStudent';
   postadcourse(body: any) {
     return this.post<any>(this.mapcourse, body)
   }
   getcourse() {
     return this.get(this.getcourseURL)
+  }
+  getuser() {
+    return this.get(this.getusers)
   }
 
 
